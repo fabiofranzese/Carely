d2b6e42549dbfd3ffc6f05345c7a0cc6148e52e7
Daniele Fontana <danyfo00@gmail.com>
2025-03-13 09:52:40 +0100

    CompletedView And Views Folder

---
+++ Carely/Views/CompletedView.swift
@@ -0,0 +1,37 @@
+//
+//  CompletedView.swift
+//  Carely
+//
+//  Created by Daniele Fontana on 13/03/25.
+//
+
+import SwiftUI
+
+struct CompletedView: View {
+    @ObservedObject var tasksList: TaskListViewModel
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(tasksList.tasks
+                    .filter { $0.isDone }
+                    .sorted(by: { $0.date.timeIntervalSince1970 < $1.date.timeIntervalSince1970 })
+                ) { task in
+                    NavigationLink(destination: TaskDetailViewCaregiver(task: task)) {
+                        VStack(alignment: .leading) {
+                            Text(task.title)
+                                .font(.headline)
+                            Text(task.description)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                            Text(task.date.description)
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Completed")
+        }
+    }
+}
